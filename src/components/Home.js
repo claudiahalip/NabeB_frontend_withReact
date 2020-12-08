ffc51fa2055dbb0cf6ed2f69c71d96283b2606e6
claudiahalip <claudia4ro@yahoo.com>
2020-12-07 23:02:30 -0600

    Home component

---
+++ src/components/Home.js
@@ -0,0 +1,23 @@
+
+import { Component } from "react"
+import React from 'react'
+import {Route} from 'react-router-dom'
+
+class Home extends Component{
+
+    render(){
+        return(
+            <div>
+                <Route exact  path = '/'>
+                <div class='header'>
+                <h1>NABE B</h1>
+                </div>
+                </Route>
+                
+            </div>
+        )
+    }
+
+}
+
+export default Home
