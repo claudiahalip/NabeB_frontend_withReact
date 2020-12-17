09b40efe8b299dca03ac60cd8b561617bf77ead0
claudiahalip <claudia4ro@yahoo.com>
2020-12-16 22:55:23 -0600

    Login axios set

--- src/components/Login.js
+++ src/components/Login.js
@@ -14,6 +14,51 @@ class Login extends Component {
         }
     }
 
+    handleChange = (event)=>{
+       this.setState({
+           [event.target.name]: event.target.value
+       })
+    }
+
+    handleSubmit = (event) => {
+        event.preventDefault()
+        const {username, email, password} = this.state
+        let user = {
+            username: username,
+            email: email,
+            password: password
+        }
+
+        axios.post('http://localhost:3001/login', {user}, {withCredentials: true})
+        .then(response =>{
+            if(response.data.logged_in){
+                this.props.handleLogin(response.data)
+                this.redirect()
+            }else{
+                this.setState({
+                    errors: response.data.errors
+                })
+            }
+        })
+        .catch(console.log('api errors:', error))
+    }
+
+    redirect = ()=>{
+       this.props.history.push('/') 
+    }
+
+    handaleErrors = ()=>{
+        return (
+            <div>
+              <ul>
+              {this.state.errors.map(error => {
+              return <li key={error}>{error}</li>
+                })}
+              </ul>
+            </div>
+          )
+    }
+
 
     render(){
         return(
